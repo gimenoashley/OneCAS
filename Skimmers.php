0d401b37d536f4f97159a40500a88f774d087819
AJ <90760739+gimenoashley@users.noreply.github.com>
2023-06-07 14:36:53 +0800

    Add files via upload

---
+++ Skimmers.php
@@ -0,0 +1,18 @@
+<?php
+	include 'connected.php';
+
+  	$sql = "SELECT * FROM acadofficers";
+  	$result =$conn->query($sql);
+
+  	echo "<td align='center'>EXECUTIVE COMMITTEE <br> </td>";
+  	if($result->num_rows>0){
+  		while($row=$result->fetch_assoc()){
+  			echo "<tr>".
+  				"<td align='center'>".$row["name"]."</td>".
+  				"<td align='center'>".$row["position"]."<br>"."</td>".
+  			"</tr>";
+  		}
+  	}
+
+
+?>
