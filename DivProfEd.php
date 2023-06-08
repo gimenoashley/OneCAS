003e1935e0c1e2c5a756244b9bb154d7981c0281
Jeff Rouzel Bat-og <96866827+JeffBat-Original@users.noreply.github.com>
2023-06-08 14:02:54 +0800

    Add files via upload

---
+++ DivProfEd.php
@@ -0,0 +1,51 @@
+<?php
+	$servername = "localhost";
+  	$username = "root";
+  	$password = "";
+  	$dbname = "divisiondatabase";
+
+	// Create connection
+  	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	$sql2 = "SELECT * FROM divisions WHERE id = 2";
+  	$results = $conn->query($sql2);
+  	$sql = "SELECT * FROM divprofs WHERE id = 2";
+  	$result = $conn->query($sql);
+
+  	if ($results->num_rows > 0) {
+  		while ($row = $results->fetch_assoc()) {
+  			echo "<div class='about'>".
+  				"<h2>ABOUT</h2>"."<p>".$row["description"]."</p>".
+  			  	"<h2>MISSION</h2>"."<p>".$row["mission"]."</p>".
+				"<h2>VISION</h2>"."<p>".$row["vision"]."</p>".
+  				"<h2>CONTACT</h2>"."<td>".$row["div_email"]."<br>".
+  				$row["fb_link"]."<br>".$row["div_contactn"]."</td>"
+  				."</div>";
+
+  		}
+  	}
+
+  	echo "<div class='officerbox'>". // Open the officers section container
+  	"<h2>EXECUTIVE COMMITTEE</h2>";
+  	if ($result->num_rows > 0) {
+  		while ($row = $result->fetch_assoc()) {
+  			echo"<div class='officer'>"
+				."<table>"
+				."<tr>".
+						"<td>".
+							"<img src='exlogo.png' id='officerPic'>".
+						"</td>".
+						"<td>".
+							"<td align='left', margin-left: '5px';>".$row["name"]."<br>".
+							$row["position"]."</td>".
+						"</td>".
+					"</tr>".
+				"</table>".
+			"</div>";
+  		}
+  	}
+ 	echo "</div>"; // Close the officers section container
+
+
+
+?>
---
+++ subd_hums.php
@@ -0,0 +1,102 @@
+<!DOCtype html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Division of Humanities</title>
+	<link rel="stylesheet" type="text/css" href="css_orgs.css">
+</head>
+<body>
+	<header>
+		<div class="heading">
+			<h1>One CAS</h1>
+			<h2>College of Arts and Sciences</h2>
+			<h3>University of the Philippines Visayas</h3>
+		</div>
+		<img src="UPVLogo.png" class="logo1">
+		<img src="CASLogo.jpg" class="logo2">
+		<div class = "navBar">
+			<ul>
+				<li><a class="navigation" href="main_home.php">Home</a>
+				<!--	 OFFICES	 -->
+				<li><a class="navigation" href="main_offices.php">Offices</a>
+					<div class="sub-menu">
+						<ul>
+							<li><a class="sub-navigation" href="subo_chancellor.php">Office of the Chancellor</a></li>
+							<li><a class="sub-navigation" href="subo_vcacadaffairs.php">Office of the Vice Chancellor for Academic Affairs</a></li>
+							<li><a class="sub-navigation" href="subo_vcadmin.php">Office of the Vice Chancellor for Administration</a></li>
+							<li><a class="sub-navigation" href="subo_vcplandev.php">Office of the Vice Chancellor for Planning and Development</a></li>
+							<li><a class="sub-navigation" href="subo_vcresearchex.php">Office of the Vice Chancellor for Research and Extension</a></li>
+							<li><a class="sub-navigation" href="subo_casdean.php">Office of the College of Arts and Science Dean</a></li>
+							<li><a class="sub-navigation" href="subo_colsec.php">Office of the College Secretary</a></li>
+							<li><a class="sub-navigation" href="subo_univreg.php">Office of the University Registrar</a></li>
+							<li><a class="sub-navigation" href="subo_studaffairs.php">Office of the Student Affairs</a></li>
+							<li><a class="sub-navigation" href="subo_casstudcouncil.php">College of Arts and Sciences Student Council</a></li>
+							<li><a class="sub-navigation" href="subo_casstudcouncil.php">University Student Council</a></li>
+							<li><a class="sub-navigation" href="subo_crs.php">CRS</a></li>
+						</ul>
+					</div>
+				<!-- 	DIVISIONS/DEPARTMENT	 -->
+				<li><a class="navigation" href="main_division.php">Division/Department</a>
+					<div class="sub-menu">
+						<ul>
+							<li><a class="sub-navigation" href="subd_chem.php">Department of Chemistry</a></li>
+							<li><a class="sub-navigation" href="subd_profed.php">Department of Professional Education</a></li>
+							<li><a class="sub-navigation" href="subd_socsci.php">Department of Social Sciences</a></li>
+							<li><a class="sub-navigation" href="subd_biosci.php">Division of Biological Sciences</a></li>
+							<li><a class="sub-navigation" href="subd_hums.php">Division of Humanities</a></li>
+							<li><a class="sub-navigation" href="subd_psm.php">Division of Physical Sciences and Mathematics</a></li>
+							<li><a class="sub-navigation" href="subd_pe.php">Department of Physical Education</a></li>
+						</ul>
+					</div>
+				<!-- 	ACADEMIC ORGANIZATIONS	 -->
+				<li><a class="navigation" href="main_acadorg.php">Academic Organizations</a>
+					<div class="sub-menu">
+						<ul>
+							<li><a class="sub-navigation" href="suba_clovers.php">CLOVERS</a></li>
+							<li><a class="sub-navigation" href="suba_elektrons.php">ELEKTRONS</a></li>
+							<li><a class="sub-navigation" href="suba_goldies.php">GOLDIES</a></li>
+							<li><a class="sub-navigation" href="suba_redbolts.php">REDBOLTS</a></li>
+							<li><a class="sub-navigation" href="suba_skimmers.php">SKIMMERS</a></li>
+						</ul>
+					</div>
+				<!-- 	STUDENT ORGANIZATIONS	 -->
+				<li><a class="navigation" href="main_studentorg.php">Student Organizations</a>
+					<div class="sub-menu">
+						<ul>
+							<li><a class="sub-navigation" href="subs_biosoc.php">UPV Biological Society</a></li>
+							<li><a class="sub-navigation" href="subs_chemsoc.php">UPV Chemistry Society</a></li>
+							<li><a class="sub-navigation" href="subs_discursus.php">UPV Discursus</a></li>
+							<li><a class="sub-navigation" href="subs_kamaragtas.php">UPV Kamaragtas</a></li>
+							<li><a class="sub-navigation" href="subs_komsai.php">UPV Komsai.org</a></li>
+							<li><a class="sub-navigation" href="subs_phpub.php">UPV PH Pub</a></li>
+							<li><a class="sub-navigation" href="subs_sakdag.php">UP Sakdag</a></li>
+							<li><a class="sub-navigation" href="subs_sociosoc.php">UPV Sociological Society</a></li>
+							<li><a class="sub-navigation" href="subs_sss.php">UPV Samahang Sikolohiya Society</a></li>
+							<li><a class="sub-navigation" href="subs_statsoc.php">UPV Statistical Society</a></li>
+							<li><a class="sub-navigation" href="subs_oecon.php">UPV Oeconomicus</a></li>
+						</ul>
+					</div>
+			</ul>
+		</div> 
+	</header>
+	<div class="orgName">
+		<img src="upvmia.jpg" class="banner">
+		<img src="logo_skimmers.jpg" class="acadLogo">
+		<h2 class="acadName">Division of Humanities</h3>
+	</div>
+	<div class="grid-container">
+			<?php
+			 include 'DivHums.php';
+			?>
+	</div>
+	
+<footer>
+	<br>
+	<div class="copyright">
+		<p>University of the Philippines Visayas, Miag-ao, Iloilo, Philippines, 5023 <br>
+			Copyright 2023
+		</p>
+	</div>
+</footer>
+
+</body>
